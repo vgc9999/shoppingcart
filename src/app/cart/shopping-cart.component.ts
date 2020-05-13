eec3bef9f4a3c9605cfb9c7a2c2e640c3c899fef
unknown <venu.angular@gmail.com>
2020-05-13 15:12:14 +0530

    data changed

--- src/app/cart/shopping-cart.component.ts
+++ src/app/cart/shopping-cart.component.ts
@@ -38,7 +38,7 @@ orderfun(eve){
       {
         id: 1,
         name: 'United Color beniton',
-        price: 499.99,
+        price: 499,
         currency: 'Rs',
         image: 'images/01.jpg',
         url: 'https://static2.jassets.com/p/Puma-917-Mid-2.0-Ind.-Blue-Sneakers-4118-935263-1-product2.jpg'
@@ -46,7 +46,7 @@ orderfun(eve){
       {
         id: 2,
         name: 'Adidas sports shoes',
-        price: 249.99,
+        price: 249,
         currency: 'Rs',
         image: 'images/02.jpg',
         url: 'http://scene7.zumiez.com/is/image/zumiez/pdp_hero/adidas-Flashback-White-%26-Black-Shoes-_272010-front-US.jpg'
@@ -54,7 +54,7 @@ orderfun(eve){
       {
         id: 3,
         name: 'Adidas',
-        price: 239.99,
+        price: 239,
         currency: 'Rs',
         image: 'images/04.jpg',
         url: 'https://images-na.ssl-images-amazon.com/images/I/810h11HFM3L._UY395_.jpg'
@@ -62,7 +62,7 @@ orderfun(eve){
       {
         id: 4,
         name: 'PUMA sports ',
-        price: 119.99,
+        price: 119,
         currency: 'Rs',
         image: 'images/06.jpg',
         url: 'http://assets.myntassets.com/assets/images/1920391/2017/6/8/11496905404701-Adidas-Men-Sports-Shoes-3121496905404588-5.jpg'
@@ -70,7 +70,7 @@ orderfun(eve){
       {
         id: 5,
         name: 'puma track',
-        price: 599.99,
+        price: 599,
         currency: 'Rs',
         image: 'images/05.jpg',
         url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg'
@@ -78,7 +78,7 @@ orderfun(eve){
       {
         id: 6,
         name: 'Adidas track',
-        price: 149.99,
+        price: 149,
         currency: 'Rs',
         image: 'images/03.jpg',
         url: 'https://cdn.shopclues.com/images/thumbnails/37609/320/320/INDUSNBLREDb14634988211480324580.jpg'
@@ -86,7 +86,7 @@ orderfun(eve){
        {
         id: 7,
         name: 'United Color beniton',
-        price: 499.99,
+        price: 499,
         currency: 'EUR',
         image: 'images/01.jpg',
         url: 'https://images-na.ssl-images-amazon.com/images/I/81t38mrch6L._UL1500_.jpg'
